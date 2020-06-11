a37e585b967a59885e36a4e8210785fc7544ec46
Anh <59912241+AnhQuan94@users.noreply.github.com>
2020-06-11 16:30:57 +0200

    Controller Artist +templates +routes

---
+++ src/Controller/ArtistController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Artist;
+
+class ArtistController extends AbstractController
+{
+    /**
+     * @Route("/artist", name="artist")
+     */
+    public function index()
+    {
+        $repository = $this->getDoctrine()->getRepository(Artist::class);
+        $artists = $repository->findAll();
+
+        return $this->render('artist/index.html.twig', [
+            'artists' => $artists,
+            'resource' => 'artistes',
+        ]);
+    }
+    /**
+     * @Route("/artist/{id}", name="artist_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Artist::class);
+        $artist = $repository->find($id);
+
+        return $this->render('artist/show.html.twig', [
+            'artist' => $artist,
+        ]);
+    }
+}
